a8ce55e0c3c57a7e79976120dc590160774ff5ba
juunis <juunis@hotmail.co.uk>
2025-09-21 19:56:02 +0300

    Small indentation change

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ resource "aws_iam_role" "listservice_lambda_role" {
     name = "listservice-lambda-role"
 
     assume_role_policy = jsonencode({
-        Version   = "2012-10-17"
+        Version = "2012-10-17"
         Statement = [{
             Action    = "sts:AssumeRole"
             Effect    = "Allow"
